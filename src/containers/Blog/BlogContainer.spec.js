661d21f0cf9722fadf155b51c34349ea2273bc8b
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-18 12:13:45 -0600

    BlogContainer tests

--- src/containers/Blog/BlogContainer.spec.js
+++ src/containers/Blog/BlogContainer.spec.js
@@ -1,36 +1,61 @@
-/* import React from 'react';
+import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { fromJS } from 'immutable';
 import sinon from 'sinon';
-import { HomeContainer, stateToProps } from './HomeContainer';
-import Home from '../../components/Home/Home';
+import { BlogContainer, stateToProps } from './BlogContainer';
+import Blog from '../../components/Blog/Blog';
+import uiService from '../../services/ui/uiService';
 
-describe('HomeContainer', () => {
-  it('renders a Home component', () => {
-    const getRandomImage = sinon.stub();
-    const wrapper = shallow(<HomeContainer getRandomImage={getRandomImage} />);
-    expect(wrapper.type()).to.equal(Home);
+describe('BlogContainer', () => {
+  describe('componentWillMount()', () => {
+    it('calls props.getPosts', () => {
+      const getPosts = sinon.stub();
+      const screen = 'small';
+      const setMenuColor = sinon.stub();
+      shallow(<BlogContainer getPosts={getPosts} screen={screen} setMenuColor={setMenuColor} />);
+      expect(getPosts).to.have.been.calledOnce;
+    });
   });
 
-  describe('componentWillMount()', () => {
-    it('calls props.getRandomImage()', () => {
-      const getRandomImage = sinon.stub();
-      shallow(<HomeContainer getRandomImage={getRandomImage} />);
-      expect(getRandomImage).to.have.been.calledOnce;
+  describe('render()', () => {
+    it('renders a Blog component', () => {
+      const getPosts = sinon.stub();
+      const screen = 'small';
+      const setMenuColor = sinon.stub();
+      const wrapper = shallow(
+        <BlogContainer getPosts={getPosts} screen={screen} setMenuColor={setMenuColor} />
+      );
+      expect(wrapper.type()).to.equal(Blog);
     });
   });
 
   describe('stateToProps()', () => {
-    it('gets state.images.activeImage from images and maps to props.image', () => {
-      const images = {
-        activeImage: 1,
-        images: [{}, {}]
+    beforeEach(() => sinon.stub(uiService, 'getScreen'));
+    afterEach(() => uiService.getScreen.restore());
+
+    it('maps state.blog.fetchingPosts to props.fetchingPosts', () => {
+      const state = {
+        blog: fromJS({ fetchingPosts: true, posts: [] })
       };
-      const state = { images: fromJS(images) };
-      const activeImage = state.images.getIn(['images', images.activeImage]);
-      expect(stateToProps(state).image).to.equal(activeImage);
+      expect(stateToProps(state).fetchingPosts).to.equal(state.blog.get('fetchingPosts'));
+    });
+
+    it('maps state.blog.posts to props.posts', () => {
+      const state = {
+        blog: fromJS({ fetchingPosts: true, posts: [] })
+      };
+      expect(stateToProps(state).posts).to.equal(state.blog.get('posts'));
+    });
+
+    it('maps uiService.getScreen(state.ui) to props.screen', () => {
+      const state = {
+        blog: fromJS({ fetchingPosts: true, posts: [] }),
+        ui: {}
+      };
+      const screen = 'large';
+      uiService.getScreen.returns(screen);
+      expect(stateToProps(state).screen).to.equal(screen);
     });
   });
 });
-*/
